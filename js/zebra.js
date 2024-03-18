3866f8e4d403ce06eb2b710c7c36955cfb3b619b
Akash Roshan Mund <75384084+akashroshanmund@users.noreply.github.com>
2024-03-18 00:59:04 -0400

    Update zebra.js

--- js/zebra.js
+++ js/zebra.js
@@ -48,7 +48,7 @@ function ZBCRCallback(type, data) {
 }
 
 function selectScanner() {
-    document.getElementById("textField").value = "WWERWEF";
+    document.getElementById("textField").value = "sfdgfwe";
 }
 
 
