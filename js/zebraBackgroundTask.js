ac86f53b3423acb9cd0222a7f9dd58cf4d9239dd
Akash Roshan Mund <75384084+akashroshanmund@users.noreply.github.com>
2024-03-22 19:04:49 -0400

    Update zebraBackgroundTask.js

--- js/zebraBackgroundTask.js
+++ js/zebraBackgroundTask.js
@@ -1,4 +1,6 @@
 self.onmessage = function(event) {
+   document.getElementById("zebraBarcodeConnected").innerHTML = "started";
   EloZebraBarcodeManager.initialize("onZebraReady");
   self.postMessage("Success");
+   document.getElementById("zebraBarcodeConnected").innerHTML = "complete";
 };
