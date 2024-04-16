62ddd06f768fc02a54b13e869774010a79ef5492
Akash Roshan Mund <75384084+akashroshanmund@users.noreply.github.com>
2024-04-16 15:10:53 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -145,9 +145,11 @@ function onZebraReady(serviceBound){
   if (serviceBound === "true"){
     document.getElementById("ZebraHeader").style.color = COLOR_GREEN
     var zebraAvailable = EloZebraBarcodeManager.isZebraBarcodeConnected();
+    let input = EloZebraBarcodeManager.getConnectedZebraScannersDetails();
+    document.getElementById("textField").value = input
     console.log("Zebra BCR is Available [" + zebraAvailable + "]");
     if(zebraAvailable == true){
-        document.getElementById("zebraBarcodeConnected").innerHTML = "Zebra Barcode Reader is Connected";
+        document.getElementById("zebraBarcodeConnected").innerHTML = "Zebra Barcode Reader is Connect";
     } else {
         document.getElementById("zebraBarcodeConnected").innerHTML = "Zebra Barcode Reader is Disconnected";
     }
